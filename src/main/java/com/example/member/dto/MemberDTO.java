382d7442fcdf2f578ee2cbbf7478481f981451de
Jun87 <g.jyj87@gmail.com>
2023-05-21 13:51:13 +0900

    5/21 13:51 공부중

--- src/main/java/com/example/member/dto/MemberDTO.java
+++ src/main/java/com/example/member/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.example.member.dto;
 
+import com.example.member.entity.MemberEntity;
 import lombok.*;
 
 @Getter
@@ -13,4 +14,13 @@ public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
     private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
 }
--- src/main/java/com/example/member/service/MemberService.java
+++ src/main/java/com/example/member/service/MemberService.java
@@ -4,7 +4,10 @@ import com.example.member.dto.MemberDTO;
 import com.example.member.entity.MemberEntity;
 import com.example.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.MemberImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,30 @@ public class MemberService {
         MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity);
 
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+
+        /*
+             1. 회원이 입력한 이메일로 DB에서 조회를 함
+             2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판정
+        */
+
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+
+        if (byMemberEmail.isPresent()) {
+            //조회 결과 있다(해당 이메일을 가진 회원 정보가 있다)
+            MemberEntity memberEntity = byMemberEmail.get();
+            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                // 비밀번호 일치
+                // entity -> dto 변화 후 리턴
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
