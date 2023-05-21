817df662a489014a7fecb5b233a595c50883a819
Jun87 <g.jyj87@gmail.com>
2023-05-21 12:19:04 +0900

    Initial commit

---
+++ src/main/java/com/example/member/entity/MemberEntity.java
@@ -0,0 +1,34 @@
+package com.example.member.entity;
+
+import com.example.member.dto.MemberDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(unique = true) //unique 제약 조건 추가
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+}
---
+++ src/main/java/com/example/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.example.member.service;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.entity.MemberEntity;
+import com.example.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDTO memberDTO) {
+
+        //1. dto-> entity 변화
+        //2. repository의 메서드 호출
+        //repository의 save메서드 호출 (조건. entity객체를 넘겨줘야 함)
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+
+    }
+}
